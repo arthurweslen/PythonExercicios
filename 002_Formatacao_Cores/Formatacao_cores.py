6273b974618dee745aacd747e49827d136fdf2a3
Arthur Weslen da Silva Barreto <arthurwesley11@gmail.com>
2021-05-14 00:46:44 -0300

    Atualização 14/05/2021

---
+++ 002_Formatacao_Cores/Formatacao_cores.py
@@ -0,0 +1,37 @@
+print('='*28)
+print(f'\033[1:7m{"Cores":=^28}\033[m')
+print('='*28)
+
+print(f'\n\033[1:7m{"Cor de Texo":^28}\033[m')
+print('\033[1:30m Código: 033[1:30 para texto Preto \033[m')
+print('\033[1:31m Código: 033[1:31 para texto Vermelho \033[m')
+print('\033[1:32m Código: 033[1:32 para texto Verde \033[m')
+print('\033[1:33m Código: 033[1:33 para texto Amarelo \033[m')
+print('\033[1:34m Código: 033[1:34 para texto Azul \033[m')
+print('\033[1:35m Código: 033[1:35 para texto Magenta \033[m')
+print('\033[1:36m Código: 033[1:36 para texto Cyan \033[m')
+print('\033[1:37m Código: 033[1:37 para texto Cinza \033[m')
+print('\033[1:90m Código: 033[1:90 para texto Cinza Escuro \033[m')
+
+print(f'\n\033[1:7m{"Cor do Funndo":^28}\033[m')
+print('\033[1:40m Código: 033[1:40 para fundo Preto \033[m')
+print('\033[1:41m Código: 033[1:41 para fundo Vermelho \033[m')
+print('\033[1:42m Código: 033[1:42 para fundo Verde \033[m')
+print('\033[1:43m Código: 033[1:43 para fundo Amarelo \033[m')
+print('\033[1:44m Código: 033[1:44 para fundo Azul \033[m')
+print('\033[1:45m Código: 033[1:45 para fundo Magenta \033[m')
+print('\033[1:46m Código: 033[1:46 para fundo Cyan \033[m')
+print('\033[1:47m Código: 033[1:47 para fundo Cinza \033[m')
+print('\033[1:100m Código: 033[1:100 para fundo Cinza Escuro \033[m')
+
+print(f'\n\033[1:7m{"Outros Exmplos":^28}\033[m')
+print('\033[1:100:41m Exemplo 1 \033[m')
+print('\033[1:36:43m Exemplo 2 \033[m')
+print('\033[4:90:46m Exemplo 3 \033[m')
+print('\033[4:7m Exemplo 4 \033[m')
+print('\033[3:36:107m Exemplo 5 \033[m')
+
+
+
+
+
